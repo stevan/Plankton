fb32818415f0686bf43df7b4c96cb1e4983183b3
Stevan Little <stevan.little@booking.com>
2015-06-30 14:18:25 +0200

    improve the constructor

--- lib/Plankton/Base.pm
+++ lib/Plankton/Base.pm
@@ -6,13 +6,13 @@ our %HAS;
 
 sub new {
     my ($class, %args) = @_;
-    my %self = %args;
+    my $self  = \%args;
     my %proto = do { no strict 'refs'; %{$class . '::HAS'} };
     foreach my $k ( keys %proto ) {
-        $self{ $k } = $proto{ $k }->() 
+        $self->{ $k } = $proto{ $k }->( $self ) 
             unless exists $args{ $k };
     }
-    return bless \%self => $class;
+    return bless $self => $class;
 }
 
 1;
