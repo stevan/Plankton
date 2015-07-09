a67773088cda95f17801a7e723448df279cb7aaa
Stevan Little <stevan.little@booking.com>
2015-07-09 16:50:08 +0200

    just some cleanup

--- lib/Plankton/Base.pm
+++ lib/Plankton/Base.pm
@@ -7,12 +7,15 @@ our %HAS;
 
 sub new {
     my ($class, %args) = @_;
+
     my $self  = \%args;
-    my %proto = do { no strict 'refs'; %{$class . '::HAS'} };
-    foreach my $k ( keys %proto ) {
-        $self->{ $k } = $proto{ $k }->( $self ) 
+    my %slots = do { no strict 'refs'; %{$class . '::HAS'} };
+
+    foreach my $k ( keys %slots ) {    
+        $self->{ $k } = $slots{ $k }->( $self )
             unless exists $self->{ $k };
     }
+
     return bless $self => $class;
 }
 
